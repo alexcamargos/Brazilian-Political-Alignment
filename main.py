ede7b4aad5e4f2eaf1be7e923482c2cc90adf464
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-10-16 20:36:43 -0300

    feat: run all services

--- main.py
+++ main.py
@@ -19,6 +19,7 @@
 
 from src.models.election_year import ElectionYear
 from src.services.download_manager import DownloadManager
+from src.services.extractor_manager import ExtractionManager
 from src.services.file_downloader import FileDownloader
 from src.utils.helpers import make_directory
 
@@ -26,10 +27,14 @@ if __name__ == '__main__':
     START_YEAR = 1990
     END_YEAR = 2024
     OUTPUT_DIR = 'downloads'
+    DATA_DIR = 'data'
 
     downloader = FileDownloader()
+    extractor = ExtractionManager()
 
     make_directory(OUTPUT_DIR)
 
     manager = DownloadManager(ElectionYear, downloader, START_YEAR, END_YEAR)
     manager.run(OUTPUT_DIR)
+
+    extractor.extract_all_files(OUTPUT_DIR, DATA_DIR)
