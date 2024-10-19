374c4a3ee6ff26ff31180419778081cb974a538f
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-10-19 14:38:07 -0300

    refactor: use of correct directory

--- src/services/transformer_csv.py
+++ src/services/transformer_csv.py
@@ -28,9 +28,11 @@ from src.utils.helpers import TSECVSDialect
 class CVSTransformer(CVSTransformerInterface):
     """Transform a CSV file with TSE data."""
 
-    def __init__(self, source_directory: str, output_file: str) -> None:
-        self.source_directory = source_directory
-        self.output_file = output_file
+    def __init__(self, source_directory: str, output_dir: str, output_file: str) -> None:
+        self.__source_directory = source_directory
+        self.__output_dir = output_dir
+        self.__output_file = output_file
+        self.__output_file_path = f'{self.__output_dir}/{self.__output_file}'
 
     def __get_csv_files(self,
                         extension: str = '*[0-9][0-9][0-9][0-9]_[A-Z][A-Z].csv',
@@ -38,9 +40,9 @@ class CVSTransformer(CVSTransformerInterface):
         """Get all CSV files in the source directory."""
 
         if scenario_filter == 'uf':
-            return glob.glob(f'{self.source_directory}/{extension}')
+            return glob.glob(f'{self.__source_directory}/{extension}')
 
-        return glob.glob(f'{self.source_directory}/*.cvs')
+        return glob.glob(f'{self.__source_directory}/*.cvs')
 
     def __read_csv_rows(self, file_path: str, file_encoding: str = 'latin-1') -> list:
         """Read a CSV file and return its content."""
@@ -60,7 +62,7 @@ class CVSTransformer(CVSTransformerInterface):
         """Write data to a CSV file."""
 
         try:
-            with open(self.output_file,
+            with open(self.__output_file_path,
                       mode='a',
                       encoding=file_encoding,
                       newline='') as file_object:
@@ -75,12 +77,12 @@ class CVSTransformer(CVSTransformerInterface):
     def merge_csv_files(self) -> None:
         """Merge all CSV files in the source directory into a single file."""
 
-        if not os.path.exists(self.source_directory) and not os.path.isdir(self.source_directory):
-            raise FileNotFoundError(f'{self.source_directory} does not exist or is not a directory.')
+        if not os.path.exists(self.__source_directory) and not os.path.isdir(self.__source_directory):
+            raise FileNotFoundError(f'{self.__source_directory} does not exist or is not a directory.')
 
         csv_files = self.__get_csv_files()
         if not csv_files:
-            raise FileNotFoundError(f'No CSV files found in {self.source_directory}.')
+            raise FileNotFoundError(f'No CSV files found in {self.__source_directory}.')
 
         for index, csv_file in enumerate(csv_files):
             csv_data = self.__read_csv_rows(csv_file, 'latin-1')
