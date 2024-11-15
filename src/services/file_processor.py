ab0159973037f7edab4e3bffc5023abd04214467
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-11-15 16:48:58 -0300

    refactor: remove apply maps for parties

--- src/services/file_processor.py
+++ src/services/file_processor.py
@@ -22,12 +22,10 @@ import glob
 import pandas as pd
 
 from src.interfaces.processor import FileProcessorInterface
-from src.utils.political_parties import (incorporated_parties,
-                                         merged_parties,
-                                         parties_that_changed_names)
 
 
 class FileProcessor(FileProcessorInterface):
+    """Process files with voting data."""
 
     def __get_csv_files(self, source_directory: str, extension: str = '*.csv',) -> list:
         """Get all CSV files in the source directory."""
@@ -37,46 +35,12 @@ class FileProcessor(FileProcessorInterface):
     def __load_csv_file(self, file_path: str) -> pd.DataFrame:
         """Load a CSV file into a pandas DataFrame."""
 
-        return pd.read_csv(file_path, sep=';', encoding='latin1')
+        return pd.read_csv(file_path, sep=';', encoding='latin-1')
 
     def __fetch_brazilian_party_positions(self) -> pd.DataFrame:
         """Load positions of Brazilian parties."""
 
-        return pd.read_csv('data/positions_brazilian_parties.csv')
-
-    def __process_incorporated_parties(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        """Incorporate parties that were incorporated."""
-
-        dataframe['SG_PARTIDO'].map(incorporated_parties)
-
-        return dataframe
-
-    def __process_merged_parties(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        """Merge parties that were merged."""
-        dataframe['SG_PARTIDO'].map(merged_parties)
-
-        return dataframe
-
-    def __process_parties_that_changed_names(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        """Rename parties that changed names."""
-
-        dataframe['SG_PARTIDO'].map(parties_that_changed_names)
-
-        return dataframe
-
-    def __apply_new_maps_for_parties(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        """Apply new maps for parties."""
-
-        # Apply maps for parties that were incorporated.
-        dataframe = self.__process_incorporated_parties(dataframe)
-
-        # Apply maps for parties that were merged.
-        dataframe = self.__process_merged_parties(dataframe)
-
-        # Apply maps for parties that changed names.
-        dataframe = self.__process_parties_that_changed_names(dataframe)
-
-        return dataframe
+        return pd.read_csv('data/positions_brazilian_parties.csv', sep=';')
 
     def __integrate_brazilian_party_roles(self, dataframe: pd.DataFrame) -> pd.DataFrame:
 
@@ -87,7 +51,7 @@ class FileProcessor(FileProcessorInterface):
         dataframe = pd.merge(dataframe,
                              positions_brazilian_parties_df,
                              left_on='SG_PARTIDO',
-                             right_on='Partido',
+                             right_on='SG_PARTIDO',
                              how='left')
 
         return dataframe
@@ -101,7 +65,8 @@ class FileProcessor(FileProcessorInterface):
 
         try:
             if file_type == 'parquet':
-                dataframe.to_parquet(f'{data_directory}/{file_name}.parquet', index=False)
+                dataframe.to_parquet(
+                    f'{data_directory}/{file_name}.parquet', index=False)
             else:
                 dataframe.to_csv(f'{data_directory}/{file_name}.csv',
                                  index=False,
@@ -117,16 +82,16 @@ class FileProcessor(FileProcessorInterface):
         for file_path in self.__get_csv_files(source_directory):
             # Get the file name to save the processed data.
             file_name = file_path.split('\\')[-1].split('.')[0]
-            print(f'Processing file: {file_path}.csv')
+            print(f'Processing file: {file_path}')
 
             # Load the CSV file with the voting data.
             voting_df = self.__load_csv_file(file_path)
-
-            # Apply new maps for parties in the DataFrame.
-            voting_df = self.__apply_new_maps_for_parties(voting_df)
 
             # Merge positions of Brazilian parties with the DataFrame.
             voting_df = self.__integrate_brazilian_party_roles(voting_df)
 
             # Save the processed election data.
-            self.__save_processed_data(voting_df, 'data', file_name, file_type='parquet')
+            self.__save_processed_data(dataframe=voting_df,
+                                       data_directory='data',
+                                       file_name=file_name,
+                                       file_type='parquet')
